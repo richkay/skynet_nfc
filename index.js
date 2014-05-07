b7b50599ef12f2e03a336c12e3d5d35751a13f98
virgilvox <mzara1@asu.edu>
2014-05-06 19:23:56 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+
+
+function Plugin(messenger, options){
+  this.messenger = messenger;
+  this.options = options;
+  return this;
+}
+
+var optionsSchema = {
+  type: 'object',
+  properties: {
+    comport: {
+      type: 'string',
+      required: true
+    }, target: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
+var messageSchema = {
+  type: 'object',
+  properties: {
+    text: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
+var com = require("serialport");
+
+var serialPort = new com.SerialPort(this.options.comport, {
+    baudrate: 9600,
+    parser: com.parsers.readline('\r\n')
+  });
+
+
+serialPort.on('open',function() {
+  console.log('Port open');
+});
+
+serialPort.on('data', function(data) {
+  console.log(data); 
+
+  this.messenger.send({devices: this.options.target, message: {nfcid: data}});
+                   
+                   
+
+   
+});
+
+/*  ARTIFACT
+
+Plugin.prototype.onMessage = function(message){
+  var data = message.message || message.payload;
+  console.log(this.options.greetingPrefix + ', ' + message.fromUuid);
+  if(message.fromUuid){
+    this.messenger.send({devices: message.fromUuid, message: {greeting: this.options.greetingPrefix + ' back atcha: ' + data.text}});
+  }
+
+}; */
+
+Plugin.prototype.destroy = function(){
+  //clean up
+  console.log('destroying.', this.options);
+};
+
+
+module.exports = {
+  Plugin: Plugin,
+  optionsSchema: optionsSchema,
+  messageSchema: messageSchema
+};
